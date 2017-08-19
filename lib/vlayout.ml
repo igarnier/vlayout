3fef37e04ad010c6c07783b903819ad1417a900f
Ilias <ilias.gar@gmail.com>
2017-08-19 18:29:03 +0200

    Added Style to exported modules.

--- lib/vlayout.ml
+++ lib/vlayout.ml
@@ -1,6 +1,7 @@
 
 module Bbox     = Bbox
 module Pt       = Pt
-module Image    = Image                    
+module Image    = Image
+module Style    = Style
 module Commands = Commands
 module Backends = Backends
